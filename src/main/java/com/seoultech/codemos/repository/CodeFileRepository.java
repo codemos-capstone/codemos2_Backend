5b86c396969b49443b9d8bf24b123322a3fd9b27
Younghyo Cho <yhcho0405@kakao.com>
2024-05-08 01:56:32 +0900

    add: Codefile repository interf

--- src/main/java/com/seoultech/codemos/repository/CodeFileRepository.java
+++ src/main/java/com/seoultech/codemos/repository/CodeFileRepository.java
@@ -1,2 +1,14 @@
-package com.seoultech.codemos.repository;public class CodeFileRepository {
+package com.seoultech.codemos.repository;
+
+import com.seoultech.codemos.model.CodeFile;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CodeFileRepository extends MongoRepository<CodeFile, String> {
+    List<CodeFile> findByUserId(String userId);
+    Optional<CodeFile> findByIdAndUserId(String id, String userId);
 }
