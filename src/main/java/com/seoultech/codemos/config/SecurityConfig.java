643c56ddb8e4732448ce22537b554ee32ad16d5f
ezcho <ezcho886@naver.com>
2024-05-08 05:06:37 +0900

    docs: security config

--- src/main/java/com/seoultech/codemos/config/SecurityConfig.java
+++ src/main/java/com/seoultech/codemos/config/SecurityConfig.java
@@ -1,36 +1,49 @@
 package com.seoultech.codemos.config;
 
-import com.seoultech.codemos.dto.TokenDto;
 import com.seoultech.codemos.jwt.TokenProvider;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import com.seoultech.codemos.service.oauth.CustomUserDetailsService;
+import com.seoultech.codemos.service.oauth.OAuth2AuthenticationSuccessHandler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 
-import java.io.PrintWriter;
 
-@RequiredArgsConstructor
+
 @Configuration
 @EnableWebSecurity
 @Component
 public class SecurityConfig {
 
     private final TokenProvider tokenProvider;
+    private final CustomUserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    @Autowired
+    public SecurityConfig(TokenProvider tokenProvider, CustomUserDetailsService userDetailsService) {
+        this.tokenProvider = tokenProvider;
+        this.userDetailsService = userDetailsService;
+    }
+
+
+    @Autowired
+    private OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+
+
+
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -40,37 +53,26 @@ public class SecurityConfig {
         //HTTP 기본 인증 비활성화, 폼로그인 비활성화
         http.formLogin(AbstractHttpConfigurer::disable);
         http.httpBasic(AbstractHttpConfigurer::disable);
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.authorizeHttpRequests(
                 authorize -> authorize
                         .requestMatchers("/home/home").permitAll()
                         .requestMatchers("/login/**").permitAll()
-                        .requestMatchers("/api/v1/code-file/**").permitAll()
-                        .requestMatchers("/api/v1/judge/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/sign").permitAll()
+                        .requestMatchers("/auth/login").permitAll()
                         .anyRequest().authenticated()
         );
-        http.oauth2Login(oauth2 -> oauth2
-                .successHandler((request, response, authentication) -> {
-                    if (authentication instanceof OAuth2AuthenticationToken) {
-                        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
-                        TokenDto tokenDto = tokenProvider.generateTokenDto(oauthToken);
 
-                        response.setContentType("application/json;charset=UTF-8");
-                        response.setStatus(HttpServletResponse.SC_OK);
-                        PrintWriter writer = response.getWriter();
-                        writer.println("{\"access_token\": \"" + tokenDto.getAccessToken() + "\", " +
-                                "\"token_type\": \"" + tokenDto.getGrantType() + "\", " +
-                                "\"expires_in\": " + tokenDto.getTokenExpiresIn() + "}");
-                        writer.flush();
-                        writer.close();
-                    } else {
-                        response.sendRedirect("/home/loginFailure"); // 이 경우는 일반적으로 발생하지 않지만 안전을 위해 처리
-                    }
-                })
+        http.oauth2Login(oauth2 -> oauth2
+                .successHandler(oAuth2AuthenticationSuccessHandler)
                 .failureHandler((request, response, exception) -> {
-                    response.sendRedirect("/home/loginFailure"); // 실패 시 이동할 경로
+                    response.sendRedirect("/google/loginFailure"); // 실패 시 이동할 경로
                 })
         );
 
         return http.build();
     }
+
 }
+
