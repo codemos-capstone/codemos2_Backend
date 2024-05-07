4979da4c05187a680c470b3a101f76a8dc3cf92c
Younghyo Cho <yhcho0405@kakao.com>
2024-05-08 01:50:57 +0900

    add: CodeFile entity

--- src/main/java/com/seoultech/codemos/model/CodeFile.java
+++ src/main/java/com/seoultech/codemos/model/CodeFile.java
@@ -1,2 +1,28 @@
-package com.seoultech.codemos.model;public class CodeFile {
+package com.seoultech.codemos.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Builder
+@Document(collection = "codeFiles")
+public class CodeFile {
+    @Id
+    private String id;
+    private String name;
+    private String content;
+    private String language;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+    private String userId;
 }
